522757eaeaf81afb992b5b41105429211fea0724
lihao <lihao@zego.im>
2022-10-31 15:35:50 +0800

    ✨ Add invitation service

---
+++ src/plugins/invitation/invitation_service.js
@@ -0,0 +1,93 @@
+import ZegoUIKitCorePlugin from '../../components/internal/ZegoUIKitCorePlugin';
+import ZegoUIKitPluginType from '../../components/internal/ZegoUIKitPluginType';
+
+const ZegoUIKitSignalingPlugin = ZegoUIKitCorePlugin.getPlugin(
+  ZegoUIKitPluginType.signaling
+);
+const ZegoUIKitInvitationService = {
+  init: (appID, appSign) => {
+    ZegoUIKitSignalingPlugin.getInstance().invoke('init', { appID, appSign });
+  },
+  uninit: () => {
+    ZegoUIKitSignalingPlugin.getInstance().invoke('uninit');
+  },
+  login: (userID, userName) => {
+    return ZegoUIKitSignalingPlugin.getInstance().invoke('login', {
+      userID,
+      userName,
+    });
+  },
+  logout: () => {
+    return ZegoUIKitSignalingPlugin.getInstance().invoke('logout');
+  },
+  sendInvitation: (invitees, timeout, type, data) => {
+    return ZegoUIKitSignalingPlugin.getInstance().invoke('sendInvitation', {
+      invitees,
+      timeout,
+      type,
+      data,
+    });
+  },
+  cancelInvitation: (invitees, data) => {
+    return ZegoUIKitSignalingPlugin.getInstance().invoke('cancelInvitation', {
+      invitees,
+      data,
+    });
+  },
+  refuseInvitation: (inviterID, data) => {
+    return ZegoUIKitSignalingPlugin.getInstance().invoke('refuseInvitation', {
+      inviterID,
+      data,
+    });
+  },
+  acceptInvitation: (inviterID, data) => {
+    return ZegoUIKitSignalingPlugin.getInstance().invoke('acceptInvitation', {
+      inviterID,
+      data,
+    });
+  },
+  onInvitationReceived: (callbackID, callback) => {
+    ZegoUIKitSignalingPlugin.getInstance().registerPluginEventHandler(
+      'callInvitationReceived',
+      callbackID,
+      callback
+    );
+  },
+  onInvitationTimeout: (callbackID, callback) => {
+    ZegoUIKitSignalingPlugin.getInstance().registerPluginEventHandler(
+      'callInvitationTimeout',
+      callbackID,
+      callback
+    );
+  },
+  onInvitationResponseTimeout: (callbackID, callback) => {
+    ZegoUIKitSignalingPlugin.getInstance().registerPluginEventHandler(
+      'callInviteesAnsweredTimeout',
+      callbackID,
+      callback
+    );
+  },
+  onInvitationAccepted: (callbackID, callback) => {
+    ZegoUIKitSignalingPlugin.getInstance().registerPluginEventHandler(
+      'callInvitationAccepted',
+      callbackID,
+      callback
+    );
+  },
+  onInvitationRefused: (callbackID, callback) => {
+    ZegoUIKitSignalingPlugin.getInstance().registerPluginEventHandler(
+      'callInvitationRejected',
+      callbackID,
+      callback
+    );
+  },
+  onInvitationCanceled: (callbackID, callback) => {
+    ZegoUIKitSignalingPlugin.getInstance().registerPluginEventHandler(
+      'callInvitationCancelled',
+      callbackID,
+      callback
+    );
+  },
+};
+
+export default ZegoUIKitInvitationService;
