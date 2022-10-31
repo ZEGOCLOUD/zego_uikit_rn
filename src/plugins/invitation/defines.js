413456409d269e61b4e456b6bcb9dc939558eeb5
lihao <lihao@zego.im>
2022-10-31 15:49:08 +0800

    📝 Add result class

---
+++ src/plugins/invitation/defines.js
@@ -0,0 +1,10 @@
+export default class ZegoInvitationServiceResult {
+  code = '';
+  message = '';
+  result = '';
+  constructor(code = '', message = '', result = '') {
+    this.code = code;
+    this.message = message;
+    this.result = result;
+  }
+}
