3da8bd3b4aca788cc50222f0a17c03cf5b2094fc
ttzhang <ttzhang@zego.im>
2022-11-25 14:45:25 +0800

    feat: audioVideoView 新增头像宽高属性avatarSize

--- src/components/audio_video/ZegoAudioVideoView/AudioFrame.js
+++ src/components/audio_video/ZegoAudioVideoView/AudioFrame.js
@@ -1,126 +1,154 @@
-import { View, StyleSheet, Text, ImageBackground } from "react-native";
-import React, { useState, useEffect } from 'react'
-import ZegoUIKitInternal from "../../internal/ZegoUIKitInternal";
+import { View, StyleSheet, Text, ImageBackground } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import ZegoUIKitInternal from '../../internal/ZegoUIKitInternal';
 
 export default function AudioFrame(props) {
-    const {
-        userInfo,
-        showSoundWave,
-        audioViewBackgroudColor,
-        audioViewBackgroudImage
-    } = props;
+  const {
+    userInfo,
+    showSoundWave,
+    audioViewBackgroudColor,
+    audioViewBackgroudImage,
+    avatarSize,
+  } = props;
 
-    const [hasSound, setHasSound] = useState(false);
+  const [hasSound, setHasSound] = useState(false);
 
-    const getShotName = (name) => {
-        if (!name) {
-            return '';
-        }
-        const nl = name.split(' ');
-        var shotName = '';
-        nl.forEach(part => {
-            if (part !== '') {
-                shotName += part.substring(0, 1);
-            }
-        });
-        return shotName;
+  const getShotName = (name) => {
+    if (!name) {
+      return '';
     }
+    const nl = name.split(' ');
+    var shotName = '';
+    nl.forEach((part) => {
+      if (part !== '') {
+        shotName += part.substring(0, 1);
+      }
+    });
+    return shotName;
+  };
 
-
-    useEffect(() => {
-        ZegoUIKitInternal.onSoundLevelUpdate('AudioFrame' + userInfo.userID, (userID, soundLevel) => {
-            if (userInfo.userID == userID) {
-                setHasSound(soundLevel > 5);
-            }
-        });
-        return () => {
-            ZegoUIKitInternal.onSoundLevelUpdate('AudioFrame' + userInfo.userID);
+  useEffect(() => {
+    ZegoUIKitInternal.onSoundLevelUpdate(
+      'AudioFrame' + userInfo.userID,
+      (userID, soundLevel) => {
+        console.log();
+        if (userInfo.userID == userID) {
+          setHasSound(soundLevel > 5);
         }
-    }, [])
-
-    return (
-        <View style={cstyle(audioViewBackgroudColor ? audioViewBackgroudColor : '#4A4B4D').container}>
-            <ImageBackground
-                source={audioViewBackgroudImage
-                    ? { uri: audioViewBackgroudImage }
-                    : null} resizeMode="cover"
-                style={styles.imgBackground}
-            >
-                {showSoundWave && hasSound ?
-                    <View style={waveStyle(164, '#515155').circleWave}>
-                        <View style={waveStyle(153, '#636266').subCircleWave} />
-                        <View style={waveStyle(141, '#6B6A71').subCircleWave} />
-                    </View> :
-                    <View />
-                }
-                <View style={styles.avatar}>
-                    <Text style={styles.nameLabel}>{getShotName(userInfo.userName)}</Text>
-                </View>
-            </ImageBackground>
-        </View>
+      }
     );
+    console.log('===audio frame', showSoundWave, hasSound);
+    return () => {
+      ZegoUIKitInternal.onSoundLevelUpdate('AudioFrame' + userInfo.userID);
+    };
+  }, []);
+
+  return (
+    <View
+      style={
+        cstyle(audioViewBackgroudColor ? audioViewBackgroudColor : '#4A4B4D')
+          .container
+      }
+    >
+      <ImageBackground
+        source={
+          audioViewBackgroudImage ? { uri: audioViewBackgroudImage } : null
+        }
+        resizeMode="cover"
+        style={styles.imgBackground}
+      >
+        {showSoundWave && hasSound ? (
+          <View style={waveStyle(avatarSize.width + 8, '#c7d6f7').circleWave}>
+            <View
+              style={waveStyle(avatarSize.width + 6, '#6093fb').subCircleWave}
+            />
+            <View
+              style={waveStyle(avatarSize.width + 4, '#3655ff').subCircleWave}
+            />
+          </View>
+        ) : (
+          <View />
+        )}
+        <View
+          style={[
+            styles.avatar,
+            {
+              width: avatarSize.width
+                ? avatarSize.width
+                : ((129 / 375) * 100).toString() + '%',
+              height: avatarSize.height
+                ? avatarSize.height
+                : ((129 / 375) * 100).toString() + '%',
+            },
+          ]}
+        >
+          <Text style={styles.nameLabel}>{getShotName(userInfo.userName)}</Text>
+        </View>
+      </ImageBackground>
+    </View>
+  );
 }
 
-const cstyle = (bgColor) => StyleSheet.create({
+const cstyle = (bgColor) =>
+  StyleSheet.create({
     container: {
-        flex: 1,
-        width: '100%',
-        height: '100%',
-        position: 'absolute',
-        justifyContent: 'center',
-        backgroundColor: bgColor,
+      flex: 1,
+      width: '100%',
+      height: '100%',
+      position: 'absolute',
+      justifyContent: 'center',
+      backgroundColor: bgColor,
     },
-})
-const waveStyle = (w, color) => StyleSheet.create({
+  });
+const waveStyle = (w, color) =>
+  StyleSheet.create({
     circleWave: {
-        flex: 1,
-        position: 'absolute',
-        alignSelf: 'center',
-        width: (w / 375 * 100).toString() + '%',
-        aspectRatio: 1,
-        borderRadius: 1000,
-        backgroundColor: color,
-        zIndex: 0,
-        justifyContent: 'center',
-        alignContent: 'center'
-
+      flex: 1,
+      position: 'absolute',
+      alignSelf: 'center',
+      width: w, //((w / 375) * 100).toString() + '%',
+      aspectRatio: 1,
+      borderRadius: 1000,
+      backgroundColor: color,
+      zIndex: 0,
+      justifyContent: 'center',
+      alignContent: 'center',
     },
     subCircleWave: {
-        flex: 1,
-        position: 'absolute',
-        alignSelf: 'center',
-        width: (w / 164 * 100).toString() + '%',
-        aspectRatio: 1,
-        borderRadius: 1000,
-        backgroundColor: color,
-        zIndex: 0,
-
+      flex: 1,
+      position: 'absolute',
+      alignSelf: 'center',
+      width: w, //((w / 164) * 100).toString() + '%',
+      aspectRatio: 1,
+      borderRadius: 1000,
+      backgroundColor: color,
+      zIndex: 0,
     },
-});
+  });
 const styles = StyleSheet.create({
-    imgBackground: {
-        flex: 1,
-        width: '100%',
-        height: '100%',
-        position: 'absolute',
-        justifyContent: 'center',
-    },
-    avatar: {
-        flex: 1,
-        width: (129 / 375 * 100).toString() + '%',
-        aspectRatio: 1,
-        borderRadius: 1000,
-        backgroundColor: '#DBDDE3',
-        position: 'absolute',
-        alignSelf: 'center',
-        alignItems: 'center',
-        justifyContent: 'center',
-        zIndex: 2,
-    },
-    nameLabel: {
-        flex: 1,
-        position: 'absolute',
-        color: '#222222',
-        fontSize: 22,
-    }
+  imgBackground: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    justifyContent: 'center',
+  },
+  avatar: {
+    flex: 1,
+    width: ((129 / 375) * 100).toString() + '%',
+    aspectRatio: 1,
+    borderRadius: 1000,
+    backgroundColor: '#DBDDE3',
+    position: 'absolute',
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 2,
+  },
+  nameLabel: {
+    flex: 1,
+    position: 'absolute',
+    color: '#222222',
+    fontSize: 22,
+  },
 });
--- src/components/audio_video/ZegoAudioVideoView/index.js
+++ src/components/audio_video/ZegoAudioVideoView/index.js
@@ -1,148 +1,151 @@
-import React, { useEffect, useState } from "react";
-import { View, StyleSheet, Text } from "react-native";
-import Delegate from "react-delegate-component";
-import AudioFrame from "./AudioFrame";
-import VideoFrame from "./VideoFrame";
-import ZegoUIKitInternal from "../../internal/ZegoUIKitInternal";
-
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import Delegate from 'react-delegate-component';
+import AudioFrame from './AudioFrame';
+import VideoFrame from './VideoFrame';
+import ZegoUIKitInternal from '../../internal/ZegoUIKitInternal';
 
 function MaskViewDefault(props) {
-    const { userInfo } = props;
-    const { userName = '' } = userInfo;
-    return (
-        <View style={styles.defaultMaskContainer}>
-            <View style={styles.defaultMaskNameLabelContainer}>
-                <Text style={styles.defaultMaskNameLabel}>{userName}</Text>
-            </View>
-        </View>
-    );
+  const { userInfo } = props;
+  const { userName = '' } = userInfo;
+  return (
+    <View style={styles.defaultMaskContainer}>
+      <View style={styles.defaultMaskNameLabelContainer}>
+        <Text style={styles.defaultMaskNameLabel}>{userName}</Text>
+      </View>
+    </View>
+  );
 }
 
 export default function ZegoVideoView(props) {
-    const {
-        userID,
-        roomID,
-        audioViewBackgroudColor,
-        audioViewBackgroudImage,
-        showSoundWave = true,
-        useVideoViewAspectFill = false,
-        foregroundBuilder
-    } = props;
+  const {
+    userID,
+    roomID,
+    audioViewBackgroudColor,
+    audioViewBackgroudImage,
+    showSoundWave = true,
+    useVideoViewAspectFill = false,
+    foregroundBuilder,
+    avatarSize,
+  } = props;
 
-    const [userInfo, setUserInfo] = useState({});
-    const [isCameraOn, setIsCameraOn] = useState(true);
+  const [userInfo, setUserInfo] = useState({});
+  const [isCameraOn, setIsCameraOn] = useState(true);
 
+  useEffect(() => {
+    const user = ZegoUIKitInternal.getUser(userID);
+    if (user) {
+      setUserInfo(user);
+      setIsCameraOn(user.isCameraDeviceOn);
+    }
+    console.log('===audio video view ', user, userInfo);
+  }, []);
 
-    useEffect(() => {
-        const user = ZegoUIKitInternal.getUser(userID);
-        if (user) {
-            setUserInfo(user);
+  useEffect(() => {
+    const callbackID = 'ZegoVideoView' + String(userID);
+
+    ZegoUIKitInternal.onSDKConnected(callbackID, () => {
+      const user = ZegoUIKitInternal.getUser(userID);
+      if (user) {
+        setUserInfo(user);
+        setIsCameraOn(user.isCameraDeviceOn);
+      }
+    });
+    ZegoUIKitInternal.onUserInfoUpdate(callbackID, (info) => {
+      if (info.userID == userID) {
+        setIsCameraOn(info.isCameraDeviceOn);
+        setUserInfo(info);
+      }
+    });
+    ZegoUIKitInternal.onRoomStateChanged(
+      callbackID,
+      (reason, errorCode, extendedData) => {
+        if (ZegoUIKitInternal.isRoomConnected()) {
+          const user = ZegoUIKitInternal.getUser(userID);
+          if (user) {
             setIsCameraOn(user.isCameraDeviceOn);
+          }
         }
-    }, [])
-
-    useEffect(() => {
-        const callbackID = 'ZegoVideoView' + String(userID);
-
-        ZegoUIKitInternal.onSDKConnected(callbackID, () => {
-            const user = ZegoUIKitInternal.getUser(userID);
-            if (user) {
-                setUserInfo(user);
-                setIsCameraOn(user.isCameraDeviceOn);
-            }
-        });
-        ZegoUIKitInternal.onUserInfoUpdate(callbackID,
-            (info) => {
-                if (info.userID == userID) {
-                    setIsCameraOn(info.isCameraDeviceOn);
-                    setUserInfo(info);
-                }
-            });
-        ZegoUIKitInternal.onRoomStateChanged(callbackID,
-            (reason, errorCode, extendedData) => {
-                if (ZegoUIKitInternal.isRoomConnected()) {
-                    const user = ZegoUIKitInternal.getUser(userID);
-                    if (user) {
-                        setIsCameraOn(user.isCameraDeviceOn);
-                    }
-                }
-            });
-        return () => {
-            ZegoUIKitInternal.onSDKConnected(callbackID);
-            ZegoUIKitInternal.onUserInfoUpdate(callbackID);
-            ZegoUIKitInternal.onRoomStateChanged(callbackID);
-        }
-    }, [])
-    return (
-        <View style={styles.container}>
-            <VideoFrame
-                style={styles.videoContainer}
-                userID={userID}
-                roomID={roomID}
-                fillMode={useVideoViewAspectFill ? 1 : 0} // 1:AspectFill, 0:AspectFit
-            >
-                {!isCameraOn ? <AudioFrame
-                    userInfo={userInfo}
-                    showSoundWave={showSoundWave}
-                    audioViewBackgroudColor={audioViewBackgroudColor}
-                    audioViewBackgroudImage={audioViewBackgroudImage}
-                /> : null}
-            </VideoFrame>
-
-            <Delegate
-                style={styles.mask}
-                to={foregroundBuilder}
-                default={MaskViewDefault}
-                props={{ userInfo }}
-            />
-        </View>
+      }
     );
+    return () => {
+      ZegoUIKitInternal.onSDKConnected(callbackID);
+      ZegoUIKitInternal.onUserInfoUpdate(callbackID);
+      ZegoUIKitInternal.onRoomStateChanged(callbackID);
+    };
+  }, []);
+  return (
+    <View style={styles.container}>
+      <VideoFrame
+        style={styles.videoContainer}
+        userID={userID}
+        roomID={roomID}
+        fillMode={useVideoViewAspectFill ? 1 : 0} // 1:AspectFill, 0:AspectFit
+      >
+        {!isCameraOn ? (
+          <AudioFrame
+            userInfo={userInfo}
+            showSoundWave={showSoundWave}
+            audioViewBackgroudColor={audioViewBackgroudColor}
+            audioViewBackgroudImage={audioViewBackgroudImage}
+            avatarSize={avatarSize}
+          />
+        ) : null}
+      </VideoFrame>
+
+      <Delegate
+        style={styles.mask}
+        to={foregroundBuilder}
+        default={MaskViewDefault}
+        props={{ userInfo }}
+      />
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: '100%',
-        height: '100%',
-        position: 'absolute',
-    },
-    videoContainer: {
-        flex: 1,
-        width: '100%',
-        height: '100%',
-        zIndex: 1,
-    },
-    mask: {
-        flex: 1,
-        width: '100%',
-        height: '100%',
-        position: 'absolute',
-        zIndex: 2,
-    },
-    defaultMaskContainer: {
-        flex: 1,
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-    },
-    defaultMaskNameLabelContainer: {
-        flex: 1,
-        backgroundColor: '#2A2A2A',
-        opacity: 0.5,
-        position: 'absolute',
-        alignSelf: 'center',
-        paddingLeft: 5,
-        paddingRight: 5,
-        paddingBottom: 3,
-        paddingTop: 3,
-        borderRadius: 6,
-        bottom: 5,
-        right: 5
-    },
-    defaultMaskNameLabel: {
-        flex: 1,
-        color: '#FFFFFF',
-        fontSize: 12
-    }
+  container: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+  },
+  videoContainer: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    zIndex: 1,
+  },
+  mask: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    zIndex: 2,
+  },
+  defaultMaskContainer: {
+    flex: 1,
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  },
+  defaultMaskNameLabelContainer: {
+    flex: 1,
+    backgroundColor: '#2A2A2A',
+    opacity: 0.5,
+    position: 'absolute',
+    alignSelf: 'center',
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingBottom: 3,
+    paddingTop: 3,
+    borderRadius: 6,
+    bottom: 5,
+    right: 5,
+  },
+  defaultMaskNameLabel: {
+    flex: 1,
+    color: '#FFFFFF',
+    fontSize: 12,
+  },
 });
