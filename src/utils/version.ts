5c58d0a7a9cd67b3357e1c8dab6aa7105593fe3a
tripli <tripli@zego.im>
2024-08-15 12:41:43 +0800

    Add callkit version info to log.

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -6,9 +6,10 @@ import { packageVersion } from './package';
 export const logComponentsVersion = (extraInfo: Map<string, string>) => {
     var expressVersionPromise = require('zego-express-engine-reactnative').default.getVersion()
     var zimVersionPromise = ZegoUIKitCorePlugin.getZIMPlugin().default.getVersion()
-    var zpnsVersionPromise = ZegoUIKitCorePlugin.getZPNsPlugin().default.getVersion()
+    var zpnsVersionPromise = ZegoUIKitCorePlugin.getZPNsPlugin() ? ZegoUIKitCorePlugin.getZPNsPlugin().default.getVersion() : ""
+    var callkitVersionPromise = ZegoUIKitCorePlugin.getCallKitPlugin() ? "unknown" : ""
 
-    Promise.all([expressVersionPromise, zimVersionPromise, zpnsVersionPromise])
+    Promise.all([expressVersionPromise, zimVersionPromise, zpnsVersionPromise, callkitVersionPromise])
     .then(versions => {
       let versionTable = new Map<string, string>(Object.entries({
         'Device': `${getBrand()} ${getModel()}`,
@@ -16,6 +17,7 @@ export const logComponentsVersion = (extraInfo: Map<string, string>) => {
         'Express': versions[0],
         'ZIM': versions[1],
         'ZPNs': versions[2],
+        'CallKit': versions[3],
         'ZegoUIKit': packageVersion()
       }));
       extraInfo.forEach((version, component) => {
