21507117f70ab4fffa5caf635f2c4671cc809dc7
tripli <tripli@zego.im>
2025-01-16 11:27:21 +0800

    Integrate the new callback callUserStateChanged.

--- src/signal_plugin/defines.ts
+++ src/signal_plugin/defines.ts
@@ -9,19 +9,3 @@ export enum CXCallEndedReason {
   AnsweredElsewhere = 4,
   DeclinedElsewhere = 5
 }
-export enum ZIMConnectionState {
-  Disconnected = 0,
-  Connecting = 1,
-  Connected = 2,
-  Reconnecting = 3
-}
-
-export enum ZIMCallUserState {
-  Inviting = 0,
-  Accepted = 1,
-  Rejected = 2,
-  Cancelled = 3,
-  Offline = 4,
-  Received = 5
-}
-
